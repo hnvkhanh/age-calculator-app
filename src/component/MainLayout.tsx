88c4144dd7f7bf596008e50e7b184f18f96c227c
hnvkhanh <hnvk101@gmail.com>
2023-08-10 17:39:05 +0700

    commit to push

--- src/component/MainLayout.tsx
+++ src/component/MainLayout.tsx
@@ -2,8 +2,9 @@ import Content from "./Content.component";
 
 const MainLayout = () => {
   return (
-    <div className="bg-lightGrey flex items-center justify-center h-[100vh]">        
+    <div className="bg-lightGrey flex flex-col items-center justify-center h-[100vh] font-poppins">        
         <Content />
+        <hr/>
     </div>
   )
 }
